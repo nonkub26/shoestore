456db06397566401e961eb4b65e59dd8d3f18241
nonkab26 <nontaphatkhonwai@gmail.com>
2022-07-03 12:55:35 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home/home_page.dart';
+import 'pages/login/login_page.dart';
+
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFFCEDDEE),
+      ),
+      routes: {
+        "/" :(context) =>  LoginPage(),
+        "homePage":(context) => HomePage(),
+      },
+    );
+  }
+}
